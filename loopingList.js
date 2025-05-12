56013db12027188d64826a1bebaf187f407576e3
johnny345 <59039745+johnny345@users.noreply.github.com>
2025-05-11 22:53:50 -0400

    Update loopingList.js
    Fix error

--- loopingList.js
+++ loopingList.js
@@ -3,11 +3,9 @@
 const songs = [
   { title: "Until", src: "Until.m4a" },
   { title: "Slowly", src: "Slowly.m4a" },
-  /*
   { title: "Quiet Song", src: "Quiet Song.m4a" },
   { title: "Imagining", src: "Imagining.m4a" },
-  { title: "Tender Love", src: "Tender Love.m4a" },
-  */  
+  { title: "Tender Love", src: "Tender Love.m4a" },   
 ];
 
 let currentSong = 0;
